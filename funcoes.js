190948aa53368247752bd47079a1412758678df1
EnzoFav <80775075+EnzoFav@users.noreply.github.com>
2021-06-21 21:47:45 -0300

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,25 @@
+function ExpCreat(){
+
+    window.location.href = "paginas/expcreat.html";
+
+}
+
+function Descoberta(){
+
+    window.location.href = "paginas/descoberta.html";
+    
+}
+
+function RacAlg(){
+
+    window.location.href = "paginas/racalg.html";
+    
+}
+
+
+
+function Menu(){
+
+    window.location.href = "../index.html";
+
+}
