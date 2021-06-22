92fc95f29e2195b931988962535199f081963255
EnzoFav <80775075+EnzoFav@users.noreply.github.com>
2021-06-21 21:39:11 -0300

    commit inicial

---
+++ Funcoes.js
@@ -0,0 +1,25 @@
+function ExpCreat(){
+
+    window.location.href = "Páginas/ExpCreat.html";
+
+}
+
+function Descoberta(){
+
+    window.location.href = "Páginas/Descoberta.html";
+    
+}
+
+function RacAlg(){
+
+    window.location.href = "Páginas/RacAlg.html";
+    
+}
+
+
+
+function Menu(){
+
+    window.location.href = "../Menu.html";
+
+}
